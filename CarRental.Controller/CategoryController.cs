2ab054e247470828e97425f46119f009647a4e7a
NataliaMZ-IT <zamperlinin@gmail.com>
2025-11-21 00:30:14 -0300

    Add feat: add, list and find category

---
+++ CarRental.Controller/CategoryController.cs
@@ -0,0 +1,123 @@
+﻿using CarRental.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils.Databases;
+
+namespace CarRental.Controller
+{
+    public class CategoryController
+    {
+        private readonly SqlConnection Connection = new(ConnectionDB.GetConnectionString());
+
+        public void AddCategory(Category category)
+        {
+            Connection.Open();
+            using (SqlTransaction transaction = Connection.BeginTransaction())
+            {
+                try
+                {
+                    var command = new SqlCommand(Category.INSERTCATEGORY, Connection, transaction);
+
+                    command.Parameters.AddWithValue("@Name", category.Name);
+                    command.Parameters.AddWithValue("@Description", category.Description ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@DailyRate", category.DailyRate);
+
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                }
+                catch (SqlException ex)
+                {
+                    transaction.Rollback();
+                    throw new Exception("Error adding category: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new Exception("Unexpected error adding category: " + ex.Message);
+                }
+                finally
+                {
+                    Connection.Close();
+                }
+            }
+        }
+
+        public List<Category> ListCategories()
+        {
+            Connection.Open();
+            try
+            {
+                var command = new SqlCommand(Category.SELECTALLCATEGORIES, Connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<Category> categories = [];
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        var category = new Category(reader["Nome"].ToString(),
+                                                    reader["Descricao"].ToString(),
+                                                    Convert.ToDecimal(reader["Diaria"]));
+
+                        categories.Add(category);
+                    }
+                }
+                return categories;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error listing categories: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unexpected error listing categories: " + ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+
+        public Category? FindCategoryByName(string name)
+        {
+            Connection.Open();
+            try
+            {
+                var command = new SqlCommand(Category.SELECTCATEGORYBYNAME, Connection);
+
+                command.Parameters.AddWithValue("@Name", name);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    if (reader.Read())
+                    {
+                        var category = new Category(reader["Nome"].ToString(),
+                                                    reader["Descricao"].ToString(),
+                                                    Convert.ToDecimal(reader["Diaria"]));
+                        category.SetCategoryID(Convert.ToInt32(reader["CategoriaID"]));
+
+                        return category;
+                    }
+                }
+                return null;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error finding category: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unexpected error finding category: " + ex.Message);
+            }
+            finally
+            {
+                Connection.Close(); 
+            }
+        }
+    }
+}
--- CarRental.Models/Category.cs
+++ CarRental.Models/Category.cs
@@ -6,13 +6,42 @@ namespace CarRental.Models
 {
     public class Category
     {
-        public int CategoryID { get; private set; }
-        public string Nome { get; private set; }
-        public string Descricao { get; private set; }
-        public decimal Diaria { get; private set; }
+        public readonly static string INSERTCATEGORY = "INSERT INTO tblCategorias VALUES (@Name, @Description, @DailyRate);";
 
-        // TODO: Create necessary set methods
-        // TODO: Create ToString override
-        // TODO: Create Controller for Category and make CRUD methods
+        public readonly static string SELECTALLCATEGORIES = @"SELECT c.Nome, c.Descricao, c.Diaria 
+                                                               FROM tblCategorias";
+
+        public readonly static string SELECTCATEGORYBYNAME = @"SELECT * FROM tblCategorias
+                                                               WHERE c.Nome = @Name";
+
+        public readonly static string UPDATECATEGORYBYNAME = "UPDATE tblCategorias WHERE Nome = @Name";
+
+        public readonly static string DELETECATEGORYBYID = "DELETE FROM tblCategorias WHERE CategoriaID = @CategoryID";
+
+        public int CategoryID { get; private set; }
+        public string Name { get; private set; }
+        public string? Description { get; private set; }
+        public decimal DailyRate { get; private set; }
+
+        public Category (string name, decimal dailyRate)
+        {
+            Name = name;
+            DailyRate = dailyRate;
+        }
+
+        public Category(string name, string? description, decimal dailyRate) : this(name, dailyRate)
+        {
+            Description = description;
+        }
+
+        public void SetCategoryID(int categoryID)
+        {
+            CategoryID = categoryID; 
+        }
+
+        public override string ToString()
+        {
+            return $"Category: {Name}\nDescription: {Description}\nDaily Rental Rate: {DailyRate}\n";
+        }
     }
 }
